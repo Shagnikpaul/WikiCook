81874c6059e3bc2b2e43ead19fa5df4aaa546d8a
Shagnik <shagnikpaul.772@gmail.com>
2026-05-13 22:14:20 +0530

    rebase frontend

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,4 +1,3 @@
-import { Button } from '#/components/ui/button.tsx';
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({ component: Home })
@@ -8,9 +7,8 @@ function Home() {
     <div className="p-8">
       <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
       <p className="mt-4 text-lg">
-        Edit <code>src/routes/index.tsx</code> to get started. ok
+        Edit <code>src/routes/index.tsx</code> to get started.
       </p>
-      <Button variant={'outline'}>Hello</Button>
     </div>
   )
 }
